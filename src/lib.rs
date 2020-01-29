5720beac643e7fc146e329cd958c4a4d277c964d
Hadrien G <knights_of_ni@gmx.com>
2020-01-29 22:34:08 +0100

    Nope. Let's just ignore that bit then :(

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@
 //! cost, which is why you will need to enable it explicitly using the "raw"
 //! [cargo feature](http://doc.crates.io/manifest.html#usage-in-end-products).
 //!
-//! ```rust
+//! ```ignore
 //! // Create and split a triple buffer
 //! use triple_buffer::TripleBuffer;
 //! let buf = TripleBuffer::new(String::with_capacity(42));
@@ -245,7 +245,7 @@ impl<T: Send> Input<T> {
     /// too magical for the target audience of this method.
     ///
     /// To use this method, you have to enable the crate's `raw` feature
-    #[cfg(any(feature = "raw", test, doctest))]
+    #[cfg(any(feature = "raw", test))]
     pub fn raw_input_buffer(&mut self) -> &mut T {
         self.input_buffer()
     }
@@ -257,7 +257,7 @@ impl<T: Send> Input<T> {
     /// an output flag which tells you whether an unread value was overwritten.
     ///
     /// To use this method, you have to enable the crate's `raw` feature
-    #[cfg(any(feature = "raw", test, doctest))]
+    #[cfg(any(feature = "raw", test))]
     pub fn raw_publish(&mut self) -> bool {
         self.publish()
     }
@@ -364,7 +364,7 @@ impl<T: Send> Output<T> {
     /// raw_update() in order to fetch buffer updates from the producer.
     ///
     /// To use this method, you have to enable the crate's `raw` feature
-    #[cfg(any(feature = "raw", test, doctest))]
+    #[cfg(any(feature = "raw", test))]
     pub fn raw_output_buffer(&mut self) -> &mut T {
         self.output_buffer()
     }
@@ -378,7 +378,7 @@ impl<T: Send> Output<T> {
     /// Return a flag telling whether an update was carried out
     ///
     /// To use this method, you have to enable the crate's `raw` feature
-    #[cfg(any(feature = "raw", test, doctest))]
+    #[cfg(any(feature = "raw", test))]
     pub fn raw_update(&mut self) -> bool {
         self.update()
     }
